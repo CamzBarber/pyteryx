4f0b18fce200f695a1061ff7f36615fe3bcf6b0b
Cameron Barber <34965004+CamzBarber@users.noreply.github.com>
2018-07-25 14:56:03 +0100

    Added headers

--- pyteryx.py
+++ pyteryx.py
@@ -1,4 +1,6 @@
+import time
 from session_id import get_session_id
+
 
 class Pyteryx(object):
 	"""docstring for AlterPyx"""
@@ -6,11 +8,21 @@ class Pyteryx(object):
 		self.hostname = host
 		self.username = user
 		self.password = pwrd
-		self.sessionID = get_session_id(self.hostname, self.username, self.password)
+		self.session_id = get_session_id(self.hostname, self.username, self.password)
+		self.headers = {
+			'Accept-Encoding': 'gzip, deflate',
+			'X-Requested-With': 'XMLHttpRequest',
+			'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+			'Accept': 'application/json, text/javascript, */*; q=0.01',
+			'X-Authorization': self.session_id,
+			'Connection': 'keep-alive',
+			'cache-control': 'no-cache',
+    		}
 
 	def get_all_workflows(workspace):
 		# gets all workflows from private/collection/district
-		pass
+		millis = int(round(time.time() * 1000))
 
 	def get_workflow_info(id):
 		# get the details of a workflow based on an ID
