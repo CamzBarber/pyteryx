ca167803508f7ac5eba5b7ac4712958be9beda48
Cameron Barber <34965004+CamzBarber@users.noreply.github.com>
2018-07-25 14:40:52 +0100

    Added code for session ID

--- session_id.py
+++ session_id.py
@@ -1,2 +1,24 @@
+# host: http://...
 def get_session_id(host, user, pwrd):
-    pass
+    headers = {
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'cache-control': 'no-cache',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    data = {'scheme': 'windows', 'parameters': [{'name': 'updateLastLoginDate', 'value': True}]}
+
+    data = json.dumps(data, separators=(',', ':'))
+
+    response = requests.post(host + '/gallery/api/auth/sessions/',
+                             auth=HttpNtlmAuth(user, pwrd),
+                             data=data,
+                             headers=headers)
+
+    session_id = 'SPECIAL ' + response.json()['sessionId']
+    return session_id
